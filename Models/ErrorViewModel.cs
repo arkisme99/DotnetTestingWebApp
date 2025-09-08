ce226e527ebbf902550be9783ca1add52b6d8142
achmadrizky99 <achmadrizky99@gmail.com>
2025-09-08 16:30:02 +0700

    first push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetTestingWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
