fa932481f83a0d27e7f89eb7e37f749a2da01cd3
achmadrizky99 <achmadrizky99@gmail.com>
2025-09-18 08:25:05 +0700

    update migration

--- Migrations/20250918012314_initial.Designer.cs
+++ Migrations/20250918012314_initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotnetTestingWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250917122702_Initial")]
-    partial class Initial
+    [Migration("20250918012314_initial")]
+    partial class initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- Migrations/20250918012314_initial.cs
+++ Migrations/20250918012314_initial.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace DotnetTestingWebApp.Migrations
 {
     /// <inheritdoc />
-    public partial class Initial : Migration
+    public partial class initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
